f324c8c5eb149e1213bbed7693494baef4c06a6f
ErwinA <acuario.erwin00@gmail.com>
2021-01-18 12:26:07 -0800

    Added Spring Boot framework

---
+++ site/src/test/java/com/loggr/site/SiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loggr.site;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
